1f01689077931a9699f0e4e3fd0421908c28cc10
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-10-19 23:39:48 +0530

    #20 Exercise 2: Solutions Added red,blue,green,yellow color palletes

--- src/App.js
+++ src/App.js
@@ -14,23 +14,38 @@ function App() {
   {/* HUM chahte ki hamari poori react website ki state ka control App.js se ho....aur iskika hum example dekhenge by enabling and disabling dark mode from App.js */ }
   const [mode, setmode] = useState("light");
 
-  let toggleMode = () => {
-    if (mode === "light") {
-      setmode("dark");
-      document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enabled", "success");
-      // document.title = "TextUtils-Dark Mode";
-    }
-    else {
-      setmode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled", "success");
-      // document.title = "TextUtils-Light Mode";
-    }
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-primary');
   }
 
+  let toggleMode = (cls) => {   // cls here is short form of class, aur isme hum bootstrap ki classes laayenge like danger,success..etc 
+    removeBodyClasses();   // calling the function
+    console.log(cls);
+    // document.body.classList.add("bg-"+cls);   // Adding bootstrap color classes to body
 
-
+    if (cls === "light") {
+              if (mode === "light") {
+                setmode("dark");
+                document.body.style.backgroundColor = "#042743";   // dekho yaha direct cls nahi likh sakte kyunki css ko nahi pata ki primary ke corresponding kaunsa color hai isliye body mein bootsrap classes ko add karo aur fir jaake backgrounds rangeen honge
+                showAlert("Dark mode has been enabled", "success");
+                // document.title = "TextUtils-Dark Mode";
+              }
+              else {
+                setmode("light");
+                document.body.style.backgroundColor = "white";
+                showAlert("Light mode has been enabled", "success");
+                // document.title = "TextUtils-Light Mode";
+              }
+    }
+    else {
+      document.body.classList.add("bg-" + cls);
+    }
+  }
 
   const [alert, setalert] = useState(null);    // alert state
   const showAlert = (message, type) => {      // type-> bootstrap ka type hai like if type is success to green color show hoga, danger-> red
@@ -51,16 +66,16 @@ function App() {
         <Alert alert={alert} />
 
         <div className="container my-3 ">
-           <Switch>
+          <Switch>
             <Route exact path="/about">      {/*use exact to Avoid confusion in situations like componenet1 --> /about and componenet2 --> /about/home*/}
               <About mode={mode} />
-             </Route>
+            </Route>
             <Route exact path="/">
               <TextForm heading="Enter the text below to analyze" mode={mode} showAlert={showAlert} />
             </Route>
           </Switch>
         </div>
-        
+
       </Router>
     </>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,19 +36,15 @@ export default function Navbar(props) {
                             </Link>
                         </li>
                     </ul>
-                    {/* <form className="d-flex">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button className="btn btn-primary" type="submit">
-                                Search
-                            </button>
-                        </form> */}            
+                     <div className="d-flex">
+                        {/* onClick={props.toggleMode('primary')} AAP AISE direct nahi kar sakte kyunki onClick ko ek function chaiye hota hai naaki function call...to app function dene ke liye just function ka naam likhdo without () and fir us function ko bahar as an arrow function banado jaise ki humne saari buttons mein kiya hai.....ya fir aap directly yahi ek arrow function banalo(jaise ki neeche kiya huya hai).....IMPORTANT jaise hi aapne ek baar function de diya onClick ko fir aap kuch bhi karo chahe to uske andar se function call karlo */}
+                         <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height:"30px", width:"30px", cursor:"pointer"}}></div>
+                         <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height:"30px", width:"30px", cursor:"pointer"}}></div>
+                         <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height:"30px", width:"30px", cursor:"pointer"}}></div>
+                         <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height:"30px", width:"30px", cursor:"pointer"}}></div>
+                     </div>
                     <div className={`form-check form-switch text-${props.mode=== "light"? 'dark': 'light'}`}>  {/*text-light is bootstrap class */}
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <input className="form-check-input" onClick={()=>{props.toggleMode('light')}} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark mode</label>
                     </div>
                 </div>
